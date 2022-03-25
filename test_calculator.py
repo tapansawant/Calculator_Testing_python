7fd4c80d53a40b464937fdd9ecb433cce06139cb
tapansawant <tapansawant45@gmail.com>
2022-03-25 15:16:37 +0530

    used setUp and TearDown

--- test_calculator.py
+++ test_calculator.py
@@ -3,29 +3,33 @@ import unittest
 
 
 class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.x = 34
+        self.y = 50
+
+    # use to cleanup the resources
+    def tearDown(self):
+        self.x = 0
+        self.y = 0
+
     def test_add(self):
-        x = 10
-        y = 20
-        result = Calculator.add(x, y)
-        self.assertEqual(result, x + y)
+        result = Calculator.add(self.x, self.y)
+        self.assertEqual(result, self.x + self.y)
 
     def test_sub(self):
-        x = 10
-        y = 20
-        result = Calculator.sub(x, y)
-        self.assertEqual(result, x - y)
+
+        result = Calculator.sub(self.x, self.y)
+        self.assertEqual(result, self.x - self.y)
 
     def test_multi(self):
-        x = 10
-        y = 20
-        result = Calculator.mult(x, y)
-        self.assertEqual(result, x * y)
+
+        result = Calculator.mult(self.x, self.y)
+        self.assertEqual(result, self.x * self.y)
 
     def test_div(self):
-        x = 10
-        y = 20
-        result = Calculator.div(x, y)
-        self.assertEqual(result, x / y)
+
+        result = Calculator.div(self.x, self.y)
+        self.assertEqual(result, self.x / self.y)
 
 
 if __name__ == "__main__":
